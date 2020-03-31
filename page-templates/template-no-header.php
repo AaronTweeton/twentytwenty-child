e7384ac9cfa1b22067e8794d7695af8927000748
Aaron Tweeton <aaron@aarontweeton.com>
2020-03-30 21:24:59 -0700

    Added "No Header" template

---
+++ page-templates/template-no-header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: No Header Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Child
+ * @since 1.0
+ */
+
+
+get_template_part( 'singular' );
